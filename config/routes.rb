bcb7dbd4231e856fc7ca042ce8ff061e24b50fc5
Jonas Zimmermann <jonas.zimmermann99@gmail.com>
2014-01-10 12:00:08 +0100

    Add a StaticPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ArchtexxApp::Application.routes.draw do
+  # root route
+  
+
+  get "static_pages/home"
+  get "static_pages/contact"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
