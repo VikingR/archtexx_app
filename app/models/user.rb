8e80fbd44bce9f3eaa576aacc127228b76f03eb2
Jonas Zimmermann <jonas.zimmermann99@gmail.com>
2014-01-14 17:16:53 +0100

    Add Session Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Before saving to the DB, lowercase the email of the current object
   before_save { self.email = email.downcase }
+  # Run method before saving
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -10,4 +12,18 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 ArchtexxApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
 
   # root route
   root 'static_pages#home'
 
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
